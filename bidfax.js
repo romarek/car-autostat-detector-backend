0aedee0ef9609b98a51f283b43c05a00ca610d8a
patryk-swierlikowski <71077406+patryk-swierlikowski@users.noreply.github.com>
2021-11-21 18:37:50 +0100

    Browser args flag

--- bidfax.js
+++ bidfax.js
@@ -9,7 +9,7 @@ async function scrapeData(url, i) {
     const browser = await puppeteer.launch(
       {
         headless: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--use-gl=egl', '--disable-dev-shm-usage', '--disable-extensions-except=' + this.extensionPathBuildPath, '--load-extension=' + this.extensionPathBuildPath]
+        args: ['--headless', '--no-sandbox', '--disable-setuid-sandbox', '--use-gl=egl', '--disable-dev-shm-usage', '--disable-extensions-except=' + this.extensionPathBuildPath, '--load-extension=' + this.extensionPathBuildPath]
       }
     );
     const page = await browser.newPage();
