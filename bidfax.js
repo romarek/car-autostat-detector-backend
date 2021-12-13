e65bfb627abec0cf3273becd3da0811f7572eb38
patryk-swierlikowski <71077406+patryk-swierlikowski@users.noreply.github.com>
2021-12-13 15:37:28 +0100

    Update bidfax.js

--- bidfax.js
+++ bidfax.js
@@ -6,7 +6,7 @@ async function scrapeData(url, i) {
     const browser = await puppeteer.launch(
       {
         headless: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--proxy-server=socks5://127.0.0.1:9050']
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
       }
     );
     const page = await browser.newPage();
